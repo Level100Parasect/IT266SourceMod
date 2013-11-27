8e310de80292aecc8a5e407eb48b5a5d248fae18
Louis <arcilla_L@yahoo.com>
2013-11-27 16:34:52 -0500

    Initial Commit

---
+++ run_hlmv.bat
@@ -0,0 +1,2 @@
+@"C:\Program Files (x86)\Steam\steamapps\common\SourceSDK\bin\source2007\bin\hlmv.exe" -game "C:\Program Files (x86)\Steam\steamapps\SourceMods\mymod" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
---
+++ run_mod.bat
@@ -0,0 +1,1 @@
+@"C:\Program Files (x86)\Steam\steamapps\common\source sdk base 2007\hl2.exe" -dev -game "C:\Program Files (x86)\Steam\steamapps\SourceMods\mymod" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
