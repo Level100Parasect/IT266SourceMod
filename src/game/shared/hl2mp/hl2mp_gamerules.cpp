026fe7719882f7542a2dd8549e1f099a0bdf3453
Louis <arcilla_L@yahoo.com>
2013-11-27 20:11:38 -0500

    Added an AK47 I think

--- src/game/shared/hl2mp/hl2mp_gamerules.cpp
+++ src/game/shared/hl2mp/hl2mp_gamerules.cpp
@@ -929,6 +929,8 @@ CAmmoDef *GetAmmoDef()
 		bInitted = true;
 
 		def.AddAmmoType("AR2",				DMG_BULLET,					TRACER_LINE_AND_WHIZ,	0,			0,			60,			BULLET_IMPULSE(200, 1225),	0 );
+		//louis
+		def.AddAmmoType("Rifle",			DMG_BULLET,					TRACER_LINE_AND_WHIZ,	0,			0,			500,		BULLET_IMPULSE(200, 1225),	0 );
 		def.AddAmmoType("AR2AltFire",		DMG_DISSOLVE,				TRACER_NONE,			0,			0,			3,			0,							0 );
 		def.AddAmmoType("Pistol",			DMG_BULLET,					TRACER_LINE_AND_WHIZ,	0,			0,			150,		BULLET_IMPULSE(200, 1225),	0 );
 		def.AddAmmoType("SMG1",				DMG_BULLET,					TRACER_LINE_AND_WHIZ,	0,			0,			225,		BULLET_IMPULSE(200, 1225),	0 );
